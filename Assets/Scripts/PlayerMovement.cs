c16e22e07d8d2c6e668b7b340dbbcf26dac09fe7
eclipse2101 <rdericieux25@aoiths.org>
2024-09-21 21:29:35 -0400

    updated on basic movement
    added the things like not being able to move if you are crouching, moving slower if u walking backwards, and fixing a few problems it had before hand

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,9 +9,14 @@ public class PlayerMovement : MonoBehaviour
    [SerializeField] public Vector2 Input; // think of this as a horziontal and vertical input method mushed together
    // Note that this type of way to get the x and y inputs for your player is good when using unity new input system as far as I know
    public Rigidbody2D  rb; 
-   public float speed; 
+   float speed; 
+   public float movespeed1; // normal movement speed
+   public float movespeed2; // moving back movement speed
+   public float movespeed3; // running movment speed 
    public float regJump; 
    public bool isFacingRight = true; 
+    bool isCrouching; 
+    bool isGrounded = false;
    private CharacterController playerController; 
 
 
@@ -24,13 +29,55 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(Input.x * speed, rb.velocity.y); 
+       MovementFunction();
+
+       if(Input.y > 0 && isGrounded == true)
+       {
+         Debug.Log("Player can jump"); 
+       }
+
     }
 
     ///////////MOVEMENT//////////////
     public void Move(InputAction.CallbackContext context)
     {
         Input.x = context.ReadValue<Vector2>().x;
+        Input.y = context.ReadValue<Vector2>().y; 
+    }
+
+    private void MovementFunction()
+    {
+       // allows the player to move left and right only if they not crouching 
+       if(Input.y < 0 && isGrounded == true)
+       {
+         isCrouching = true; 
+       }
+       else
+       {
+        transform.Translate(Vector2.right * Input.x * Time.deltaTime * speed);
+        isCrouching = false; 
+       }
+
+       if(isFacingRight == true && Input.x < 0)
+       {
+         speed = movespeed2; 
+       }
+       else if(isFacingRight == false && Input.x > 0)
+       {
+          speed = movespeed2; 
+       }
+       else
+       {
+         speed = movespeed1; 
+       }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if (collision.gameObject.CompareTag("Ground"))
+        {
+             isGrounded = true;
+        }
     }
 
     ///////ATTACKING/////////
